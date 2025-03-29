5da4125e9da2cae8d2e37f2e10e5fa297de65530
M-MSHAIKH <shaikhmohammadaadil11@gmail.com>
2025-03-29 16:10:39 +0100

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,115 @@
+# Predicting Tire-Road Friction Coefficient Under High Steering Excitation
+The tire friction coefficient plays a crucial role in determining a vehicle's ability to maneuver. In most driving situations, the friction forces between the tires and the road contribute significantly to the net forces acting on the vehicle, thereby influencing both its lateral and longitudinal movements. Consequently, it is vital for drivers, driver assistance systems, or autonomous vehicles to accurately estimate the friction coefficient that governs the interaction between the tires and the roadway. ([1,2])
+
+This project focuses on predicting the tire-road friction coefficient, i.e. \mu for a stationary vehicle under high steering excitation conditions using deep learning models, i.e. Long Short-Term Memory (LSTM) and Gated Recurrent Units (GRU). The models are trained with labeled (True) values of the friction coefficient (μ) obtained from a physical tire brush model, utilizing mean values to represent tire-road interactions accurately. The previous research on the tire brush models indicates that the physical is failed to capture the and stay consistent constant value of $\mu$ during high steering excitation.
+
+Further, for simplicity the model predict the $\mu$ of front tires. By capturing the temporal dependencies in the data, this approach enhances the prediction of the friction coefficient for stationary vehicles, contributing to better tire-road interaction models for various applications in vehicle dynamics and stability analysis. 
+
+## Data
+The following figures illustrate high steering excitations. For this project, only data from 10 seconds to 40.50 seconds is used.
+![steering angles](steering_angles.png)
+![steering torques](steering_torque.png)
+
+## Input and Output of the model
+
+- **Input**:
+    - Left steering angles $\delta_l(t)$
+    - right steering angles $\delta_r(t)$
+    - Left steering torques
+    - Right steering torques
+    - Left steering rate $\dot\delta_l(t)$
+    - Right steering rate $\dot\delta_r(t)$
+
+- **Output**:
+    - $\mu$
+
+## Labeled $\mu$ values
+
+- **Taking the mean of physical model, i.e. Tire Brush model**:
+    - Wet surface: 0.715519
+    - Ice sheet surface: 0.246479
+    - Dry surface: 0.504774
+    - Ice surface: 0.181976
+
+## LSTM Architecture in MATLAB
+
+- **Network Configuration**:
+  - Predicts 1 response.
+  - 6 features per time step.
+  - 100 hidden units in LSTM layer.
+  - Training: 500 epochs, mini-batches of 200.
+
+- **Layers**:
+  - Sequence Input Layer: 6 features.
+  - LSTM Layer: 100 hidden units.
+  - Fully Connected Layer: Maps to 1 response.
+
+- **Training Options**:
+  - Optimizer: Adam.
+  - L2 Regularization: 1e-04.
+  - Gradient Threshold: 10.
+  - No shuffling.
+  - Visualizes training progress.
+  - Learning rate drops every 100 epochs.
+  - Validation every 30 epochs.
+
+- **Training Function**:
+    - trainnet
+
+
+## GRU Architecture in MATLAB
+
+- **Network Configuration**:
+  - Predicts 1 response.
+  - 6 features per time step.
+  - 200 hidden units in GRU layer.
+  - Training: 500 epochs, mini-batches of 100.
+
+- **Layers**:
+  - Sequence Input Layer: 6 features.
+  - GRU Layer: 200 hidden units.
+  - Fully Connected Layer: Maps to 1 response.
+
+- **Training Options**:
+  - Optimizer: Adam.
+  - L2 Regularization: 1e-04.
+  - Gradient Threshold: 10.
+  - No shuffling.
+  - Visualizes training progress.
+  - Learning rate drops every 100 epochs.
+  - Validation every 30 epochs.
+
+This concise summary highlights the key aspects of the GRU architecture and its training configuration.
+
+- **Training Function**:
+    - trainnet
+
+
+## Prediction Using LSTM
+![lstm predictions](lstm_predictions.png)
+
+## Prediction using the GRU
+![gru predictions](gru_predictions.png)
+
+## Accuracy
+
+- **LSTM accuracy**:
+    - Root Mean Square Error : 0.1014
+    - Mean Absolute Percentage Error: 16.9859
+
+- **GRU accuracy**:
+    - Root Mean Square Error : 0.1579
+    - Mean Absolute Percentage Error: 21.3703
+
+## Result
+
+-   The LSTM model outperformed the GRU model in terms of both RMSE and MAPE, indicating better overall accuracy and reliability in predicting the coefficient of friction under the given conditions.
+
+-   The GRU model's higher error rates and increased variability in predictions suggest it may be less suitable for this specific task compared to the LSTM model.
+
+-    LSTM model, Exhibited overlapping predictions for different surfaces, particularly between dry and wet surfaces, and ice and ice sheet surfaces. This overlap makes surface classification more challenging with the LSTM model.
+
+## References:
+[1] Pacejka, Hans. Tire and vehicle dynamics. Elsevier, 2005.
+
+[2] Beal, Craig E., and Sean Brennan. "Friction detection from stationary steering manoeuvres." Vehicle System Dynamics 58.11 (2020): 1736-1765.
---
+++ gru_22_03.m
@@ -0,0 +1,289 @@
+% Creating the the model which takes input 4 files(Wet, Ice, Ice Sheet, Dry) and
+% Predict they all surafces friction coefficinet
+% dividing this into train test dataset
+% training from 10sec, from where the actual experiement begin until 40
+
+% Using only one value of mu. (a mean value of mu for each file or
+% datasets). so the mu will be constnat through entire datasets.
+%%%%%%%%%%%%%%%%%%%   LSTM   %%%%%%%%%%%%%%%%%%%%%
+
+clear all
+% close all
+clc
+
+% Load all craig files
+
+load_ice = load('hal_2018-12-12_ae.mat');
+load_wet = load('hal_2018-12-12_ad.mat');
+load_sheet = load('hal_2018-12-12_ai.mat');
+load_dry = load('hal_2018-12-12_ac.mat');
+
+%% Training set
+
+% creating input datasets all files
+
+% First Ice then wet then ICE cubes this ensures better optimization as ice
+% and ice sheets gives you the small values, if both trained at the end
+% then weights will not optimized well for dry surface.
+
+% For consistency of the datalength we assume that we predict the friction
+% until 40.500 sec or 20,251 time steps
+% chnaging the position of the testing datasets
+a=2;
+b=3;
+c=4;
+d=1;
+num_start = 5001;   %start from the 10 sec
+num_train = floor((20251 - 5001) * 0.70);
+num_valid = num_train + floor((20251 - 5001) * 0.10);   %start validation data point
+num_steps = 20251;
+left_steering_angles = [load_ice.y(num_start:num_steps,99),load_wet.y(num_start:num_steps,99),load_sheet.y(num_start:num_steps,99),load_dry.y(num_start:num_steps,99)];
+L_angle_train = vertcat(left_steering_angles(1:num_train,1),left_steering_angles(1:num_train,2),left_steering_angles(1:num_train,3),left_steering_angles(1:num_train,4));
+L_angle_valid = vertcat(left_steering_angles(num_train+1:num_valid,1),left_steering_angles(num_train+1:num_valid,2),left_steering_angles(num_train+1:num_valid,3),left_steering_angles(num_train+1:num_valid,4));
+L_angle_test = vertcat(left_steering_angles(num_valid+1:end,a),left_steering_angles(num_valid+1:end,b),left_steering_angles(num_valid+1:end,c),left_steering_angles(num_valid+1:end,d));
+
+right_steering_angle = [load_ice.y(num_start:num_steps,100),load_wet.y(num_start:num_steps,100),load_sheet.y(num_start:num_steps,100),load_dry.y(num_start:num_steps,100)];
+R_angle_train = vertcat(right_steering_angle(1:num_train,1),right_steering_angle(1:num_train,2),right_steering_angle(1:num_train,3),right_steering_angle(1:num_train,4));
+R_angle_valid = vertcat(right_steering_angle(num_train+1:num_valid,1),right_steering_angle(num_train+1:num_valid,2),right_steering_angle(num_train+1:num_valid,3),right_steering_angle(num_train+1:num_valid,4));
+R_angle_test = vertcat(right_steering_angle(num_valid+1:end,a),right_steering_angle(num_valid+1:end,b),right_steering_angle(num_valid+1:end,c),right_steering_angle(num_valid+1:end,d));
+
+left_steering_torques = [load_ice.y(num_start:num_steps,116),load_wet.y(num_start:num_steps,116),load_sheet.y(num_start:num_steps,116),load_dry.y(num_start:num_steps,116)];
+L_torque_train = vertcat(left_steering_torques(1:num_train,1),left_steering_torques(1:num_train,2),left_steering_torques(1:num_train,3),left_steering_torques(1:num_train,4));
+L_torque_valid = vertcat(left_steering_torques(num_train+1:num_valid,1),left_steering_torques(num_train+1:num_valid,2),left_steering_torques(num_train+1:num_valid,3),left_steering_torques(num_train+1:num_valid,4));
+L_torque_test = vertcat(left_steering_torques(num_valid+1:end,a),left_steering_torques(num_valid+1:end,b),left_steering_torques(num_valid+1:end,c),left_steering_torques(num_valid+1:end,d));
+
+right_steering_torques = [load_ice.y(num_start:num_steps,117),load_wet.y(num_start:num_steps,117),load_sheet.y(num_start:num_steps,117),load_dry.y(num_start:num_steps,117)];
+R_torque_train = vertcat(right_steering_torques(1:num_train,1),right_steering_torques(1:num_train,2),right_steering_torques(1:num_train,3),right_steering_torques(1:num_train,4));
+R_torque_valid = vertcat(right_steering_torques(num_train+1:num_valid,1),right_steering_torques(num_train+1:num_valid,2),right_steering_torques(num_train+1:num_valid,3),right_steering_torques(num_train+1:num_valid,4));
+R_torque_test = vertcat(right_steering_torques(num_valid+1:end,a),right_steering_torques(num_valid+1:end,b),right_steering_torques(num_valid+1:end,c),right_steering_torques(num_valid+1:end,d));
+
+time_sec = [load_ice.t(num_start:num_steps),load_wet.t(num_start:num_steps),load_sheet.t(num_start:num_steps),load_dry.t(num_start:num_steps)];
+time_secIN = vertcat(load_ice.t(1:num_steps),load_wet.t(1:num_steps),load_sheet.t(1:num_steps),load_dry.t(1:num_steps));
+
+gradient_time = 0.0020; 
+delta_dot_iceL = gradient(left_steering_angles(:,1)) ./ gradient_time;
+
+
+delta_dot_iceR = gradient(right_steering_angle(:,1)) ./ gradient_time;
+delta_dot_wetL = gradient(left_steering_angles(:,2)) ./ gradient_time;
+delta_dot_wetR = gradient(right_steering_angle(:,2)) ./ gradient_time;
+delta_dot_sheetL = gradient(left_steering_angles(:,3)) ./ gradient_time;
+delta_dot_sheetR = gradient(right_steering_angle(:,3)) ./ gradient_time;
+delta_dot_dryL = gradient(left_steering_angles(:,4)) ./ gradient_time;
+delta_dot_dryR = gradient(right_steering_angle(:,4)) ./ gradient_time;
+left_ddot = [delta_dot_iceL,delta_dot_wetL,delta_dot_sheetL,delta_dot_dryL];
+L_ddot_train = vertcat(left_ddot(1:num_train,1),left_ddot(1:num_train,2),left_ddot(1:num_train,3),left_ddot(1:num_train,4));
+L_ddot_valid = vertcat(left_ddot(num_train+1:num_valid,1),left_ddot(num_train+1:num_valid,2),left_ddot(num_train+1:num_valid,3),left_ddot(num_train+1:num_valid,4));
+L_ddot_test = vertcat(left_ddot(num_valid+1:end,a),left_ddot(num_valid+1:end,b),left_ddot(num_valid+1:end,c),left_ddot(num_valid+1:end,d));
+
+right_ddot = [delta_dot_iceR,delta_dot_wetR,delta_dot_sheetR,delta_dot_dryR];
+R_ddot_train = vertcat(right_ddot(1:num_train,1),right_ddot(1:num_train,2),right_ddot(1:num_train,3),right_ddot(1:num_train,4));
+R_ddot_valid = vertcat(right_ddot(num_train+1:num_valid,1),right_ddot(num_train+1:num_valid,2),right_ddot(num_train+1:num_valid,3),right_ddot(num_train+1:num_valid,4));
+R_ddot_test = vertcat(right_ddot(num_valid+1:end,a),right_ddot(num_valid+1:end,b),right_ddot(num_valid+1:end,c),right_ddot(num_valid+1:end,d));
+
+
+% Calculating mu
+mu_ice = 0.181976 * ones(num_steps-num_start,1);
+mu_wet = 0.715519 * ones(num_steps-num_start,1);
+mu_sheet = 0.246479 * ones(num_steps-num_start,1);
+mu_dry = 0.504774 * ones(num_steps-num_start,1);
+mu_train = vertcat(mu_ice(1:num_train,1),mu_wet(1:num_train,1),mu_sheet(1:num_train,1),mu_dry(1:num_train,1));
+mu_valid = vertcat(mu_ice(num_train+1:num_valid,1),mu_wet(num_train+1:num_valid,1),mu_sheet(num_train+1:num_valid,1),mu_dry(num_train+1:num_valid,1));
+% mu_test = vertcat(mu_ice(num_valid+1:end),mu_wet(num_valid+1:end),mu_sheet(num_valid+1:end),mu_dry(num_valid+1:end));
+mu_test = vertcat(mu_wet(num_valid+1:end),0.181976,mu_sheet(num_valid+1:end),0.715519,mu_dry(num_valid+1:end),0.246479,mu_ice(num_valid+1:end),0.504774);
+
+
+% Input and output datset
+validIN_data = [L_angle_valid,R_angle_valid, L_torque_valid, R_torque_valid, L_ddot_valid,R_ddot_valid];
+validOUT_data = mu_valid;
+testIN_data = [L_angle_test,R_angle_test, L_torque_test, R_torque_test, L_ddot_test,R_ddot_test];
+testOUT_data = mu_test;
+%% %% Normalization
+norm_left_steering_angleIN = normalize(L_angle_train,'range',[-1,1]);
+min_langle = min(L_angle_train);
+max_langle = max(L_angle_train);
+norm_right_steering_angleIN = normalize(R_angle_train,'range',[-1,1]);
+min_rangle = min(R_angle_train);
+max_rangle = max(R_angle_train);
+norm_left_ddotIN = normalize(L_ddot_train,'range',[-1,1]);
+min_lddot = min(L_ddot_train);
+max_lddot = max(L_ddot_train);
+norm_right_ddotIN = normalize(R_ddot_train,'range',[-1,1]);
+min_rddot = min(R_ddot_train);
+max_rddot = max(R_ddot_train);
+norm_left_steering_torquesIN = normalize(L_torque_train,'range',[-1,1]);
+min_ltorques = min(L_torque_train);
+max_ltorques = max(L_torque_train);
+norm_right_steering_torquesIn = normalize(R_torque_train,'range',[-1,1]);
+min_rtorques = min(R_torque_train);
+max_rtorques = max(R_torque_train);
+norm_mu = normalize(mu_train,'range',[-1,1]);
+min_mu = min(mu_train);
+max_mu = max(mu_train);
+
+% Input and output training datasets
+input_data_train = [norm_left_steering_angleIN,norm_right_steering_angleIN, norm_left_ddotIN, norm_right_ddotIN, norm_left_steering_torquesIN,norm_right_steering_torquesIn];
+% input_data_train = input_data_train';
+output_data_train = norm_mu;
+% output_data_train = output_data_train';
+
+%% Normalizing the validation and the test data
+% use -4 for test data output
+
+% Normlaizing the validation data
+norm_left_steering_angle_valid = zeros(length(L_angle_valid),1);
+norm_right_steering_angle_valid = zeros(length(L_angle_valid),1);
+norm_left_ddot_valid = zeros(length(L_angle_valid),1);
+norm_right_ddot_valid = zeros(length(L_angle_valid),1);
+norm_left_steering_torques_valid = zeros(length(L_angle_valid),1);
+norm_right_steering_torques_valid = zeros(length(L_angle_valid),1);
+
+for i = 1:length(L_angle_valid)
+    norm_left_steering_angle_valid(i) = rangeRescale(L_angle_valid(i),min_langle,max_langle,-1,1);
+    norm_right_steering_angle_valid(i) = rangeRescale(R_angle_valid(i),min_rangle,max_rangle,-1,1);
+    norm_left_ddot_valid(i) = rangeRescale(L_ddot_valid(i),min_lddot,max_lddot,-1,1);
+    norm_right_ddot_valid(i) = rangeRescale(R_ddot_valid(i),min_rddot,max_rddot,-1,1);
+    norm_left_steering_torques_valid(i) = rangeRescale(L_torque_valid(i),min_ltorques,max_ltorques,-1,1);
+    norm_right_steering_torques_valid(i) = rangeRescale(R_torque_valid(i),min_rtorques,max_rtorques,-1,1);
+end
+
+input_valid = [norm_left_steering_angle_valid,norm_right_steering_angle_valid, norm_left_ddot_valid,norm_right_ddot_valid, norm_left_steering_torques_valid, norm_right_steering_torques_valid];
+% input_valid = input_valid';
+output_valid = mu_valid;
+
+% Normlaizing the test data
+norm_left_steering_angle_test = zeros(length(L_angle_test),1);
+norm_right_steering_angle_test = zeros(length(L_angle_test),1);
+norm_left_ddot_test = zeros(length(L_angle_test),1);
+norm_right_ddot_test = zeros(length(L_angle_test),1);
+norm_left_steering_torques_test = zeros(length(L_angle_test),1);
+norm_right_steering_torques_test = zeros(length(L_angle_test),1);
+
+for i = 1:length(L_angle_test)
+    norm_left_steering_angle_test(i) = rangeRescale(L_angle_test(i),min_langle,max_langle,-1,1);
+    norm_right_steering_angle_test(i) = rangeRescale(R_angle_test(i),min_rangle,max_rangle,-1,1);
+    norm_left_ddot_test(i) = rangeRescale(L_ddot_test(i),min_lddot,max_lddot,-1,1);
+    norm_right_ddot_test(i) = rangeRescale(R_ddot_test(i),min_rddot,max_rddot,-1,1);
+    norm_left_steering_torques_test(i) = rangeRescale(L_torque_test(i),min_ltorques,max_ltorques,-1,1);
+    norm_right_steering_torques_test(i) = rangeRescale(R_torque_test(i),min_rtorques,max_rtorques,-1,1);
+end
+
+input_test = [norm_left_steering_angle_test,norm_right_steering_angle_test, norm_left_ddot_test,norm_right_ddot_test, norm_left_steering_torques_test, norm_right_steering_torques_test];
+% input_test = input_test';
+output_test = mu_test;
+%% LSTM Architecture
+
+numResponses = 1;
+featureDimension = 6;
+numHiddenUnits = 100;
+maxEpochs = 500;
+miniBatchSize = 200;
+% sigmoidLayer
+Networklayers = [
+    sequenceInputLayer(featureDimension)
+    gruLayer(numHiddenUnits)
+    fullyConnectedLayer(numResponses)
+];
+%L2 Regularization (also known as Weight Decay) is a technique used to reduce overfitting by adding a penalty term to the loss function that discourages large weights. This helps prevent the model from fitting too closely to the training data and improves generalization to unseen data.
+
+% options = trainingOptions('adam', ...
+%     'MaxEpochs',maxEpochs,...
+%     'L2Regularization',1e-6,...
+%     'GradientThreshold',10,...
+%     'Metrics',"mse", ...
+%     'ObjectiveMetricName',"loss", ...
+%     'OutputNetwork',"best-validation",...
+%     'MiniBatchSize',miniBatchSize,...
+%     'Shuffle','never',...
+%     'Plots','training-progress',...
+%     'LearnRateSchedule','piecewise',...
+%     'LearnRateDropPeriod',100,...
+%     'LearnRateDropFactor',0.8,...
+%     'Verbose',0,...
+%     'ValidationData', [{input_valid} {output_valid}],...
+%     'ValidationFrequency',20);
+
+% Customizing the learning rate
+% schedule = piecewiseLearnRate( ...
+%     DropFactor=0.3, ...
+%     Period=50, ...
+%     FrequencyUnit="iteration");
+
+options = trainingOptions('adam', ...
+    'L2Regularization',1.0000e-04,...
+    'MaxEpochs',maxEpochs, ...
+    'MiniBatchSize',miniBatchSize, ...
+    'GradientThreshold',10, ...
+    'Shuffle','never', ...
+    'Plots','training-progress',...
+    'LearnRateSchedule','piecewise',...
+    'LearnRateDropPeriod',100,...
+    'Verbose',0,...
+    'ValidationData', [{input_valid} {output_valid}],...
+    'ValidationFrequency',30);
+
+%Training a LSTM
+gru_net = trainnet(input_data_train,output_data_train,Networklayers,'l1loss',options);
+
+%% Predictions
+
+% Loading the previously trained LSTM
+% load_gru = load("gru_22_03_01_02_PM.mat");
+% gru_net = load_gru.gru_net;
+%info = load_lstm.info;
+
+% view(net_train);
+y_norm = predict(gru_net,input_test);
+% y_norm = y_norm';
+
+% Renormalized to its standard value using the training min max
+y_output = zeros(length(y_norm),1);
+for s = 1:length(y_norm)
+    y_output(s,1) = (((1 + y_norm(s,1)) / 2) * (max_mu - min_mu)) + min_mu;
+end
+
+%% figure;
+
+% Time vector for the plot
+time_plot = time_sec(num_valid+1:end,1);
+time_plot = vertcat(time_plot,time_plot,time_plot,time_plot);
+figure;
+plot(y_output,'.');
+hold on;
+plot(output_test,'.')
+title('\mu estimation using GRU')
+% xlabel('Time(sec)')
+ylabel('\mu estimated')
+% xlim([10,40])
+legend('NN outputs of \mu', 'True values of \mu')
+grid on;
+
+%% WEt,shet,dry,ice
+% Time vector for the plot
+time_plot1 = time_sec(num_valid+1:end,1);
+num_val_indi = num_valid/4 + 1;
+
+h1 = figure;
+hold on;
+plot(time_plot1,y_output(1:num_val_indi),'.','Color','b');
+plot(time_plot1,y_output(num_val_indi+1 : num_val_indi*2),'.','Color','g');
+plot(time_plot1,y_output(num_val_indi*2+1 : num_val_indi*3),'.','Color','r');
+plot(time_plot1,y_output(num_val_indi*3+1 : end),'.','Color','m');
+plot(time_plot1,output_test(1:num_val_indi),'*','Color','b');
+plot(time_plot1,output_test(num_val_indi+1 : num_val_indi*2),'*','Color','g');
+plot(time_plot1,output_test(num_val_indi*2+1 : num_val_indi*3),'*','Color','r');
+plot(time_plot1,output_test(num_val_indi*3+1 : end),'*','Color','m');
+title('\mu estimation using GRU')
+% xlabel('Time(sec)')
+ylabel('\mu estimated')
+xlabel('Time (sec)')
+% xlim([10,40])
+legend('GRU prediction for wet surface','GRU prediction for ice sheet surface',...
+    'GRU prediction for dry surface','GRU prediction for ice surface',...
+    'True values for wet surface', 'True values for ice sheet surface',...
+    'True values for dry surface','True values for ice surface')
+ylim([0 1.2])
+grid on;
+%exportgraphics(h1,'gru_predictions.png','Resolution',300)
+
+
